ad8e373c0a02e884c77fa964103de14ae9be3fe1
Grenzen <grenzentur@rambler.ru>
2020-09-06 18:13:39 +0300

    complite what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//! доделать отлов ошибок и разобраться с последним пунктом с ловушками
+
+module.exports = function getSeason(date) {
+
+  if (date === undefined) {
+    return `Unable to determine the time of year!`;
+  } else if (date instanceof Date === false) {
+    throw new Error('Ooops');
+  } else if (date.getUTCFullYear()) {
+    let month = date.getMonth();
+    
+    return  result = month === 11 || month >= 0 && month <= 1 ?
+    `winter` : month >= 2 && month <= 4 ? 
+    `spring` : month >= 5 && month <= 7 ?
+    `summer` : `autumn`;
+  };
 };

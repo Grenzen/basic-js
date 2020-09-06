8e4c305331e7b73e9cd4f1f7dce7630ef9e090ce
Grenzen <grenzentur@rambler.ru>
2020-09-06 13:04:48 +0300

    complite count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  
+  //! variant 1 isn't limited to 30ms, it's 56ms
+  // const result = matrix.flat().filter(item => item === '^^');
+  // return result.length; - 56ms
+
+  //! variant 2 isn't limited to 30ms, it's 55ms
+  // let i = [];
+
+  // if (matrix.length === 0) {
+	// 	return 0;
+	// }
+
+	// const res = matrix.reduce(function(acc, curr) {
+		
+	// 	if (typeof acc !== 'undefined') {
+	// 		i = [...i, ...acc];
+	// 	};
+	// 	i = [...i, ...curr];
+	// });
+  // return i.filter(item => item === '^^').length;
+  
+  //!variant 3 is limited to 30ms
+
+  let count = 0;
+    
+  for(let i = 0; i < matrix.length; i++) {
+    for (let y = 0; y < matrix[i].length; y++) {
+      if (matrix[i][y] === '^^') {
+        count++;
+      }
+    };
+  };
+  return count;
 };

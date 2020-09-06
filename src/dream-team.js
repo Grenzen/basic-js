1675e61921f9adc6e0adcd1618070a8402ad3223
Grenzen <grenzentur@rambler.ru>
2020-09-06 15:51:05 +0300

    complite dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let crypt = [];
+
+  if (typeof members !== 'object' || 
+      members === null ||
+      members[0] === undefined) {
+		return false;
+  } else {
+	  for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] !== 'string') continue;
+		  let trimmed = members[i].trim();
+		  crypt.push(trimmed[0].toUpperCase());
+	  };
+    return crypt.sort().join('');
+  };
 };

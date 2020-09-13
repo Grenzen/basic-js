da3413ec87e48bc7ddd73cc19ec7a17b92c8abfa
Grenzen <grenzentur@rambler.ru>
2020-09-13 17:53:10 +0300

    complite recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  calculateDepth = (arr) => {
+
+    if (Array.isArray(arr)) {
+      if (arr[0] === undefined) {
+        return 1;
+      }
+      return 1 + Math.max(...arr.map(this.calculateDepth));
+    }
+    return 0;
+  };
 };

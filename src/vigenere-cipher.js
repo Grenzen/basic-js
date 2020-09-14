f0c4652b70e886b97e30c9043f84956a7fb80276
Grenzen <grenzentur@rambler.ru>
2020-09-14 16:34:26 +0300

    Complite vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(bool) {
+    this.direction = bool;
   }
+  encrypt(message, key) {
+    if (arguments.length < 2) { 
+    throw new Error('Two parameters are mandatory');
+    } else {
+      return this.vigenere(message.toLowerCase(), key);
+    }
+  }    
+  decrypt(encryptedMessage, key) {
+    if (arguments.length < 2) {
+      throw new Error('Two parameters are mandatory');
+    } else {
+      return this.vigenere(encryptedMessage, key);
+    }
+  }
+
+  vigenere(str, key) {
+    let rep = key;
+    let result = [];
+    let spaceCounter = 0;
+  
+    if (key.length < str.length) {
+      rep = key.repeat(Math.ceil(str.length / key.length));
+    }
+    rep = rep.slice(0, str.length).toLowerCase().split('').map(el => el.charCodeAt() - 97);
+
+    str.split('')
+       .map(el => el.charCodeAt())
+       .forEach((el, i) => {
+          let curr = el;
+  
+          if (el < 65 || el > 90 && el < 97 || el > 122) {
+            spaceCounter++;
+          };
+  
+          if (el > 96 && el < 123) {
+            curr = (el - 96) + rep[i - spaceCounter];
+  
+            if (curr > 26) {
+              curr = curr - 26;
+            };
+            curr = curr + 64;
+          };
+  
+          if (el > 64 && el < 91) {
+            curr = el - rep[i - spaceCounter];
+
+            if (curr < 65) {
+              curr = curr + 26;
+            }
+          }
+          result.push(curr);
+       });
+
+       if (this.direction === false) {
+        return result.map(el => String.fromCharCode(el)).reverse().join('');
+       } else {
+        return result.map(el => String.fromCharCode(el)).join('');
+       }
+  };
 }
 
 module.exports = VigenereCipheringMachine;

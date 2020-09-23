4e0f2546ec755d4683f0fede3b7f8b3f7d7d9470
Grenzen <grenzentur@rambler.ru>
2020-09-23 11:56:22 +0300

    feat: fix all updates

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,4 +3,5 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  //2n - 1 <- turns, seconds ?
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+
+  const disNext = '--discard-next';
+  const disPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+
+  if (!Array.isArray(arr)) {
+    throw new Error('arr is not an Array');
+  } else if (!arr.includes(disNext) &&
+             !arr.includes(disPrev) &&
+             !arr.includes(doubleNext) &&
+             !arr.includes(doublePrev)) {
+    return arr;
+  } else {
+
+    let transformed = arr.slice(0);
+
+    transformed.forEach((el, i) => {
+      if (i === 0 && el === disPrev ||
+          i === 0 && el === doublePrev) {
+        transformed.splice(i, 1);
+      } else if (i === transformed.length - 1 && el === disNext ||
+                 i === transformed.length - 1 && el === doubleNext) {
+        transformed.splice(i);
+      } else if (el === disNext) {
+        transformed.splice(i, 2);
+      } else if (el === disPrev) {
+        transformed.splice(i - 1, 2);
+      } else if (el === doubleNext) {
+        transformed.fill(el[i + 1], i, i + 1);
+      } else if (el === doublePrev) {
+        transformed.fill(arr[i - 1], i, i + 1);
+      }
+    });
+
+    return transformed;
+  };
 };
